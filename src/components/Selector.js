441df77299871a83481866116ac8e3e0e3ff5e39
Nicolas <nicolas.michel.lava@gmail.com>
2020-09-04 18:43:59 +0200

    Uglify a bit

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -5,28 +5,34 @@ import styles from './Selector.module.scss';
 const Selector = ({list, onSelect, onCancel}) => {
   const { t } = useTranslation();
   const [selected, setSelected] = useState('');
+  const [selectedId, setSelectedId] = useState(null);
 
   const onListEntryClicked = (id) => {
     setSelected(list[id]);
+    setSelectedId(id);
   }
 
   return (
     <div className={`${styles.Selector}`}>
       <div className={`${styles.List}`}>
-        {list.map((item, idx) =>  <SelectorListEntry key={idx} id={idx} text={item} onSelected={onListEntryClicked} />)}
+        {list.map((item, idx) => <SelectorListEntry key={idx} id={idx} text={item} onSelected={onListEntryClicked} isActive={selectedId === idx}/>)}
       </div>
 
-      <input value={selected} onChange={(e) => setSelected(e.target.value) }/>  
+      <div className={`${styles.SelectFileBar}`}>
+        File <input value={selected} onChange={(e) => setSelected(e.target.value) }/>  
+      </div>
 
-      <div onClick={() => {console.log('ACTION !'); onSelect(selected)}}>Select</div>
-      <div onClick={onCancel}>Cancel</div>
+      <div className={`${styles.ButtonBar}`}>
+        <div onClick={() => onSelect(selected)}>Select</div>
+        <div onClick={onCancel}>Cancel</div>
+      </div>
     </div>
   );
 }
 
-const SelectorListEntry = ({text, id, onSelected}) => {
+const SelectorListEntry = ({text, id, onSelected, isActive}) => {
   return (
-    <div className={`${styles.ListItem}`} onClick={() => onSelected(id)}>{text}</div>
+    <div className={`${styles.ListItem} ${isActive ? styles.active : ""}`} onClick={() => onSelected(id)}>{text}</div>
   );
 }
 
